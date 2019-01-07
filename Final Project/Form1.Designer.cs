2bcee9dcf6f8d61d9f50339400ad8d5b5a59223e
ChrisJR3 <chrirous315@ed.amdsb.ca>
2019-01-07 14:48:50 -0500

    Add project files.

---
+++ Final Project/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Final_Project
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainScreenLabel = new System.Windows.Forms.Label();
+            this.playGameButton = new System.Windows.Forms.Button();
+            this.leaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mainScreenLabel
+            // 
+            this.mainScreenLabel.AutoSize = true;
+            this.mainScreenLabel.BackColor = System.Drawing.Color.Transparent;
+            this.mainScreenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainScreenLabel.ForeColor = System.Drawing.Color.White;
+            this.mainScreenLabel.Location = new System.Drawing.Point(265, 83);
+            this.mainScreenLabel.Name = "mainScreenLabel";
+            this.mainScreenLabel.Size = new System.Drawing.Size(144, 25);
+            this.mainScreenLabel.TabIndex = 0;
+            this.mainScreenLabel.Text = "Main Screen";
+            // 
+            // playGameButton
+            // 
+            this.playGameButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.playGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playGameButton.ForeColor = System.Drawing.Color.White;
+            this.playGameButton.Location = new System.Drawing.Point(270, 132);
+            this.playGameButton.Name = "playGameButton";
+            this.playGameButton.Size = new System.Drawing.Size(127, 38);
+            this.playGameButton.TabIndex = 1;
+            this.playGameButton.Text = "Play Game";
+            this.playGameButton.UseVisualStyleBackColor = false;
+            this.playGameButton.Click += new System.EventHandler(this.playGameButton_Click);
+            // 
+            // leaveButton
+            // 
+            this.leaveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.leaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leaveButton.ForeColor = System.Drawing.Color.White;
+            this.leaveButton.Location = new System.Drawing.Point(285, 193);
+            this.leaveButton.Name = "leaveButton";
+            this.leaveButton.Size = new System.Drawing.Size(94, 34);
+            this.leaveButton.TabIndex = 2;
+            this.leaveButton.Text = "Leave";
+            this.leaveButton.UseVisualStyleBackColor = false;
+            this.leaveButton.Click += new System.EventHandler(this.leaveButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(681, 450);
+            this.Controls.Add(this.leaveButton);
+            this.Controls.Add(this.playGameButton);
+            this.Controls.Add(this.mainScreenLabel);
+            this.Name = "mainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mainScreenLabel;
+        private System.Windows.Forms.Button playGameButton;
+        private System.Windows.Forms.Button leaveButton;
+    }
+}
+
---
+++ Final Project/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project
+{
+    public partial class mainForm : UserControl
+    {
+        Boolean leftArrowDown, downArrowDown, rightArrowDown, upArrowDown, bDown, nDown, mDown, spaceDown;
+        int playX, playY, playSize, playSpeed;
+        SolidBrush heroBrush = new SolidBrush(Color.Black);
+
+        public mainForm()
+        {
+            InitializeComponent();
+            InitializeGameValues();
+        }
+
+        public void InitializeGameValues()
+        { 
+            playX = 100;
+            playY = 100;
+            playSize = 20;
+            playSpeed = 5;
+        }
+
+        private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape && gameTimer.Enabled)
+            {
+                gameTimer.Enabled = false;
+                rightArrowDown = leftArrowDown = upArrowDown = downArrowDown = false;
+
+                DialogResult result = PauseForm.Show();
+
+                if (result == DialogResult.Cancel)
+                {
+                    gameTimer.Enabled = true;
+                }
+                else if (result == DialogResult.Abort)
+                {
+                    mainForm.ChangeScreen(this, "MenuScreen");
+                }
+            }
+
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Space:
+                    spaceDown = true;
+                    break;
+                case Keys.M:
+                    mDown = true;
+                    break;
+            }
+        }
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void playGameButton_Click(object sender, EventArgs e)
+        {
+            mainScreenLabel.Visible = false;
+            playGameButton.Visible = false;
+            leaveButton.Visible = false;
+        }
+
+        private void leaveButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
